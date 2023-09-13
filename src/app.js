39a628e1154b95f570989cd4f92afbe7b8067adb
Omar Jesús Maniás <ojmanias1985@gmail.com>
2023-09-13 09:10:51 -0300

    actualizo cambios parciales

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
+const MongoStore = require('connect-mongo');
 const sessionsRouter = require('./routes/sessions');
 const viewsRouter = require('./routes/views');
+const User = require('./models/User');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://omanias:1234562023@cluster0.3lmci0d.mongodb.net/?retryWrites=true&w=majority', {
@@ -12,22 +15,73 @@ mongoose.connect('mongodb+srv://omanias:1234562023@cluster0.3lmci0d.mongodb.net/
     useUnifiedTopology: true,
 });
 
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-    session({
-        secret: 'coderhouse',
-        resave: false,
-        saveUninitialized: true,
-    })
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://omanias:1234562023@cluster0.3lmci0d.mongodb.net/?retryWrites=true&w=majority',
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+        ttl: 600,
+    }),
+    secret: 'coderhouse',
+    resave: false,
+    saveUninitialized: true,
+})
 );
 
 app.engine("handlebars", handlebars.engine())
-app.set("views", __dirname + 'routes/views')
+app.set("views", __dirname + '/views')
 app.set("view engine", "handlebars")
 
 
 app.use('/api/sessions', sessionsRouter);
-app.use('/', viewsRouter);
+// app.use('/', viewsRouter);
+
+
+app.post("/register", async (req, res) => {
+    try {
+        const { first_name, last_name, email, age, password } = req.body;
+
+        const user = new User({ first_name, last_name, email, age, password });
+        await user.save();
+
+        console.log('Usuario creado con éxito.')
+
+        console.log(user)
+
+        res.redirect('/profile');
+    } catch (error) {
+        console.error('Error al registrar el usuario:', error);
+        res.status(500).send('Error al registrar el usuario.');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
+
+
+    const { first_name, last_name, email, age } = req.session.user;
+    res.render('/profile', { first_name, last_name, email, age });
+
+    console.log('Usuario logueado con éxito.')
+
+});
+
+
+app.get('/login', (req, res) => {
+    // Check if the user is authenticated (you can replace this with your authentication logic)
+    if (!req.session.user) {
+        // If the user is not authenticated, render the login page
+        res.render('login');
+    } else {
+        // If the user is authenticated, render the profile page
+        res.render('/views/profile');
+    }
+});
+
 
 app.listen(8080, () => {
     console.log('Servidor en ejecución en el puerto 8080');
