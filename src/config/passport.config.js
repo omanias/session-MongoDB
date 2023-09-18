fed2095454e8ac6d0281eedf082822897545c5c8
Omar Jesús Maniás <ojmanias1985@gmail.com>
2023-09-18 20:33:57 -0300

    reconfig passport

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,59 +1,74 @@
-const passport = require("passport")
-const local = require("passport-local")
-const userService = require("../models/User")
-const { createHash, isValidatePassword } = require("../../utils")
+const passport = require("passport");
+const local = require("passport-local");
+const userService = require("../models/User");
+const { createHash, isValidatePassword } = require("../../utils");
 
-const localStrategy = local.Strategy
+const localStrategy = local.Strategy;
 
 const initializePassport = () => {
-    passport.use("register", new localStrategy(
-        { passReqToCallback: true, usernameField: "email" }, async (req, username, password, done) => {
-            const { first_name, last_name, email, age } = req.body
-            try {
-                let user = await userService.findOne({ email: username })
-                if (user) {
-                    console.log("El usuario ya existe")
-                    return done(null, false)
+    passport.use(
+        "register",
+        new localStrategy(
+            { passReqToCallback: true, usernameField: "email" },
+            async (req, username, password, done) => {
+                const { first_name, last_name, email, age } = req.body;
+                try {
+                    let user = await userService.findOne({ email: username });
+                    if (user) {
+                        console.log("El usuario ya existe");
+                        return done(null, false);
+                    }
+
+                    if (!first_name || !last_name || !email || !age || !password) {
+                        // Check if all required fields are present in the request body
+                        console.log("Faltan campos obligatorios");
+                        return done(null, false);
+                    }
+
+                    const newUser = {
+                        first_name,
+                        last_name,
+                        email,
+                        age,
+                        password: createHash(password), // Make sure createHash is defined
+                    };
+                    let result = await userService.create(newUser);
+                    return done(null, result);
+                } catch (error) {
+                    return done("Error al obtener el usuario " + error);
                 }
-                const newUser = {
-                    first_name,
-                    last_name,
-                    email,
-                    age,
-                    password: createHash(password)
-                }
-                let result = await userService.create(newUser)
-                return done(null, result)
-            } catch (error) {
-                return done("Error al obtener el usuario " + error)
             }
-        }
-    ))
+        )
+    );
 
     passport.serializeUser((user, done) => {
-        done(null, user._id)
-    })
+        done(null, user._id);
+    });
 
     passport.deserializeUser(async (id, done) => {
-        let user = await userService.findById(id)
-        done(null, user)
-    })
+        let user = await userService.findById(id);
+        done(null, user);
+    });
 
-    passport.use("login", new localStrategy({ usernameField: "email" }, async (username, password, done) => {
-        try {
-            const user = await userService.findOne({ email: username })
-            if (!user) {
-                return done(null, false)
+    passport.use(
+        "login",
+        new localStrategy({ usernameField: "email" }, async (username, password, done) => {
+            try {
+                const user = await userService.findOne({ email: username });
+                if (!user) {
+                    return done(null, false);
+                }
+
+                if (!isValidatePassword(password, user.password)) { // Make sure isValidatePassword is defined
+                    return done(null, false);
+                }
+
+                return done(null, user);
+            } catch (error) {
+                return done(error);
             }
-            if (!isValidatePassword(password, user.passport)) {
-                return done(null, false)
-            }
+        })
+    );
+};
 
-        } catch (error) {
-            return done(error)
-        }
-    }))
-}
-
-
-module.exports = initializePassport
+module.exports = initializePassport;
